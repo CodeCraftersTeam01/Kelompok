4dfc6e4db6a1ec472016df5f57ada61b4e2cdb62
Arjuna Lanang <wardilanang46@gmail.com>
2025-10-04 19:26:35 +0700

    penambahan contact

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const typedTextSpan = document.getElementById('typed-text');
     const cursorSpan = document.getElementById('cursor');
 
-    const textArray = ["Arjuna Lanang Adiwarsana", "Tegar Mahardika", "Esy Aulia Tri Anata", "Moh Yassar Maulana"];
+    const textArray = ["Arjuna Lanang Adiwarsana", "Tegar Mahardika", "Esy Aulia Tri Ananta", "Ahmad Yasar Maulana Dais"];
     const typingDelay = 100;
     const erasingDelay = 50;
     const newTextDelay = 2000; // Delay before starting to erase
@@ -33,4 +33,38 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // start the typing effect
     setTimeout(type, newTextDelay);
+
+    document.querySelectorAll('input[type=radio]').forEach(radio => {
+        radio.checked = false; // kosongkan semua
+    });
+
+    const radios = document.querySelectorAll('.card input[type=radio]');
+    const cards = document.querySelectorAll('.card');
+
+    radios.forEach(radio => {
+        radio.addEventListener('click', function () {
+            if (this.wasChecked) {
+                // kalau diklik lagi -> uncheck & reset semua card
+                this.checked = false;
+                cards.forEach(card => card.classList.remove('blurred'));
+            }
+            // simpan status terakhir
+            this.wasChecked = this.checked;
+        });
+
+        radio.addEventListener('change', function () {
+            // hanya jalan kalau memang ada radio yang checked
+            if (this.checked) {
+                cards.forEach(card => {
+                    if (card.querySelector('input[type=radio]').checked) {
+                        card.classList.remove('blurred'); // yang dipilih jelas
+                    } else {
+                        card.classList.add('blurred'); // yang lain blur
+                    }
+                });
+            }
+        });
+    });
+
+
 });
